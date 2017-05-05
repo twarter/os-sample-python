8e18c9402d513baa275e2e5c73f56ecdb74712c1
twarter <nbohn.econ@gmail.com>
2017-05-05 19:09:29 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!, how are you?"
+    return "Hello World!, how are you? My name is Tom"
      for v in range (0,3):
             print(v)
 
